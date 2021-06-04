809c823730915d20398ef89e79e6ea5642f2b0d4
EC2 Default User <ec2-user@ip-172-31-45-99.us-east-2.compute.internal>
2021-06-04 10:07:01 +0000

    PHP/Laravel 11 Bladeテンプレートの継承とLaravel Mixの使い方を活用してみよう

---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>tech boost</title>
+    </head>
+    <body>
+        <h1>はじめてのWEBサイトへようこそ</h1>
+    </body>
+</html>　
